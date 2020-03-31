03e8343651b119f3cbcaae2436f38fb066c4c5fa
Gilson <gilson.miranda@usp.br>
2020-03-30 22:21:27 -0300

    atualização front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());g
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3000);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Header from 'react';
+import './global.css';
+import Routes from './routes'
 /*JSX html integrado ao javascript*/
 /* Componente é uma função que retorna HTML */
 function App() {
   return (
-    <Header />
+    <div>
+      <Routes/>
+    </div>
   );
 }
 
